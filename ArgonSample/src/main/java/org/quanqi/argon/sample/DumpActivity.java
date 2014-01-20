c8b458c730c18706241be0b783e3491b6288694c
Jing Quanqi <jing@quanqi.org>
2014-01-20 17:39:59 +0800

    miao

--- ArgonSample/src/main/java/org/quanqi/argon/sample/DumpActivity.java
+++ ArgonSample/src/main/java/org/quanqi/argon/sample/DumpActivity.java
@@ -1,25 +1,23 @@
 package org.quanqi.argon.sample;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class DumpActivity extends Activity implements AdapterView.OnItemClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
         ListView listview = new ListView(this);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,android.R.layout.simple_expandable_list_item_1);
+                this, android.R.layout.simple_expandable_list_item_1);
         adapter.add("Breath");
         listview.setAdapter(adapter);
         listview.setOnItemClickListener(this);
@@ -28,7 +26,7 @@ public class DumpActivity extends Activity implements AdapterView.OnItemClickLis
 
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         startActivity(new Intent(this, BreathAnimationSample.class));
     }
 }
